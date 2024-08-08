1df106fdb712c3734cbe440147366d481301108b
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-08-08 10:34:25 +0100

    Added Home, Main and Footer Components

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,4 +1,4 @@
-function Main() {
+function MainContent() {
   return (
     <main>
       <p>I love to visit New York, Paris, and Tokyo.</p>
@@ -6,4 +6,4 @@ function Main() {
   );
 }
 
-export default Main;
+export default MainContent;
