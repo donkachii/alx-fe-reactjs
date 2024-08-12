c5152024353655f171458b57483a4f3df1e808d0
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-08-12 16:12:23 +0100

    Added Context API

---
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function UserDetails() {
+  const userData = React.useContext();
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
