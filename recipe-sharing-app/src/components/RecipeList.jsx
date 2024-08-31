15009918a367969f558549293e8d027dfd1e20c7
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-08-31 06:19:22 +0100

    Added filtered state

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,11 +1,13 @@
 import RecipeDetails from "./RecipeDetails";
 import { useRecipeStore } from "./recipeStore";
+import { SearchBar } from "./SearchBar";
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const recipes = useRecipeStore((state) => state.filteredRecipes);
 
   return (
     <div>
+      <SearchBar />
       {recipes.map((recipe) => (
         <div key={recipe.id}>
           <RecipeDetails recipeId={recipe.id} />
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,7 @@ import create from "zustand";
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: "",
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
@@ -14,5 +15,14 @@ export const useRecipeStore = create((set) => ({
   deleteRecipe: (recipeId) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+    })),
+
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filteredRecipes: [],
+  filterRecipes: () =>
+    set((state) => ({
+      filteredRecipes: state.recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+      ),
     })),
 }));
