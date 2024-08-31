f5fa7107700d752bcfc58ebe576fda932a822073
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-08-31 06:25:42 +0100

    Added filtered state

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,5 @@
 import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
 import AddRecipeForm from "./components/AddRecipeForm";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<RecipeList />} />
           <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
         </Routes>
       </Router>
     </>
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,6 +1,7 @@
-import RecipeDetails from "./RecipeDetails";
 import { useRecipeStore } from "./recipeStore";
 import { SearchBar } from "./SearchBar";
+
+import { Link } from "react-router-dom";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.filteredRecipes);
@@ -9,9 +10,12 @@ const RecipeList = () => {
     <div>
       <SearchBar />
       {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <RecipeDetails recipeId={recipe.id} />
-        </div>
+        <Link key={recipe.id} to={`/recipe/${recipe.id}`}>
+          <div>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
