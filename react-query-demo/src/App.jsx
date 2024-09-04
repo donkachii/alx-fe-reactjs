378423fc167ad748d0e4f0f115d0ed3817147197
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-09-04 12:52:30 +0100

    Added React Query page

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,7 +1,16 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import PostsComponent from "./components/PostsComponent";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      keepPreviousData: true,
+      staleTime: 60000,
+      cacheTime: 600000,
+    },
+  },
+});
 
 function App() {
   return (
