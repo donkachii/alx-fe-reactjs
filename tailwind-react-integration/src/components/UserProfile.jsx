a1c6d786433111251b5edd23df1664b68865478c
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-09-08 23:29:09 +0100

    Added Taiwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
   return (
-    <div className="max-w-sm p-8 mx-auto my-20 bg-gray-100 rounded-lg shadow-lg">
+    <div className="mx-auto my-20 bg-gray-100 rounded-lg shadow-lg md:max-w-xs sm:max-w-sm sm:p-4 md:p-8">
       <img
-        className="mx-auto rounded-full w-36 h-36"
+        className="mx-auto rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36"
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="my-4 text-xl text-blue-800">John Doe</h1>
-      <p className="text-base text-gray-600">
+      <h1 className="my-4 text-blue-800 sm:text-lg md:text-xl">John Doe</h1>
+      <p className="text-gray-600 md:text-base sm:text-sm">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
