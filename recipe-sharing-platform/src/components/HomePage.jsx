47188550e8ed59f1463a2c500c8075441a278b59
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-09-10 03:17:17 +0100

    Added Recipe Card

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,19 +10,21 @@ const HomePage = () => {
 
   return (
     <div>
-      <div className="text-center">
-        <h1 className="">Recipe Sharing Platform</h1>
+      <div className="mb-4 text-center">
+        <h1 className="mb-4 text-3xl md:text-5xl">Recipe Sharing Platform</h1>
         <p>Explore our collection of recipes:</p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
         {recipes &&
           recipes?.map((recipe) => (
             <div
-              className="flex flex-col justify-between p-4 m-4 duration-300 border border-gray-300 rounded-md cursor-pointer hover:scale-105 hover:shadow-md"
+              className="flex flex-col justify-between gap-3 p-4 m-4 duration-300 border border-gray-300 rounded-md cursor-pointer hover:scale-105 hover:shadow-md"
               key={recipe.id}
             >
-              <h2>{recipe.title}</h2>
-              <p>{recipe.summary}</p>
+              <h2 className="text-xl md:text-2xl md:font-semibold">
+                {recipe.title}
+              </h2>
+              <p className="text-sm md:text-lg">{recipe.summary}</p>
               <img
                 className="w-full h-auto"
                 src={recipe.image}
